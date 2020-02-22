4d1af96bff3de49067e26f3f0f196bafe66c4786
Vicky Hawley <49642354+vickyhawley@users.noreply.github.com>
2020-02-22 20:38:15 +0000

    Add files via upload

---
+++ C# Practice/arrays.cs
@@ -0,0 +1,26 @@
+using system;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            console.WriteLine(numbers[0]);
+            console.WriteLine(numbers[1]);
+            console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Jack", "John", "Mary"};
+        }
+    }
+}
