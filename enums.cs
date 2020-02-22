7c1d6e5fa9d30faa5d7a21f338748fce21b2d4c2
Vicky Hawley <49642354+vickyhawley@users.noreply.github.com>
2020-02-22 21:09:07 +0000

    Add files via upload

---
+++ enums.cs
@@ -0,0 +1,25 @@
+namespace CSharpFundamentals
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var method = ShippingMethod.Express;
+          System.Console.WriteLine((int)method);
+
+          var methodId = 3;
+          Console.WriteLine((ShippingMethod)methodId);
+
+          Console.WriteLine(method.ToString());
+
+          var methodName = "Express";
+          var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
