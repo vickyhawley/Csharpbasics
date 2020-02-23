7e04c9dc992dbeda9da541ae7f3d4b6d0b76561f
Vicky Hawley <49642354+vickyhawley@users.noreply.github.com>
2020-02-23 10:05:46 +0000

    Add files via upload

---
+++ foreacharray.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] {1, 2, 3, 4};
+
+            foreach (var number in numbers)
+            {
+                System.Console.WriteLine(number);
+            }
+        }
+    }
+}
