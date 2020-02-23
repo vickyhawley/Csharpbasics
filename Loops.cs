1560db342d9400a23329972729185a2b42475004
Vicky Hawley <49642354+vickyhawley@users.noreply.github.com>
2020-02-23 09:48:44 +0000

    Add files via upload

---
+++ Loops.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i = 1; 1 <= 10; i++)
+            {
+                 if(i%2 == 0)
+                 {
+                     System.Console.WriteLine(i);
+                 }
+            }
+
+            for (var i = 10; i >= 1; i--)
+            {
+                if(i%2 == 0)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+
+
+        }
+
+    }
+}
