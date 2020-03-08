c8ed1c5b3cb21957dd5068ff75c6940f962c0cfa
Vicky Hawley <49642354+vickyhawley@users.noreply.github.com>
2020-03-08 11:07:57 +0000

    Add files via upload

---
+++ BoolExp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BoolExp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int apples = 6;
+            int oranges = 9;
+            decimal applePrice = 12.60m;
+            decimal orangePrice = 4.50m;
+
+            Console.WriteLine(apples == oranges);
+            Console.WriteLine(apples != oranges);
+
+            Console.WriteLine($"{apples} > {oranges} is {apples > oranges}");
+            Console.WriteLine($"{apples} < {oranges} is {apples < oranges}");
+            Console.WriteLine($"{apples} >= {oranges} is {apples >= oranges}");
+            Console.WriteLine($"{apples} <= {oranges} is {apples <= oranges}");
+            Console.WriteLine($"{apples} is int is {apples is int}");
+
+            Console.WriteLine($"Reducing apple cost: {(apples > oranges) && (applePrice > orangePrice)}");
+
+            Console.WriteLine($"Reducing apple cost: {(apples > oranges) || (applePrice > orangePrice)}");
+
+        }
+    }
+}
